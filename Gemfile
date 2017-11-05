e30f6557ef10127fa05ca57e3f0c0004953dcf83
shimobayashi <akimasashimobayashi@gmail.com>
2017-11-06 00:04:45 +0900

    登録フィードの取得処理を並列に行うように改善した。

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ gem 'thin'
 gem 'feed-normalizer'
 gem 'mongoid', '~> 3.0'
 gem 'haml'
+gem 'em-http-request'
+gem 'async_sinatra'
 
 group :development do
     gem 'rerun'
--- app.rb
+++ app.rb
@@ -1,11 +1,14 @@
 require 'sinatra'
 require 'feed-normalizer'
-require 'open-uri'
 require 'rss/maker'
 require 'mongoid'
 require 'haml'
+require 'em-http-request'
+require 'sinatra/async'
 
 require_relative 'lib/models/settings'
+
+register Sinatra::Async
 
 Mongoid.load!('config/mongoid.yml')
 
@@ -31,28 +34,28 @@ post '/settings' do
   redirect base_url
 end
 
-get '/feed' do
-  (entries, errors) = get_entries_and_errors
+aget '/feed' do
+  get_entries_and_errors do |entries, errors|
+    rss = RSS::Maker.make('2.0') do |rss|
+      rss.channel.title = "#{get_settings.filtering_regexp_str} - turifo"
+      rss.channel.description = errors.size > 0 ? errors.join("\n") : 'There is no error.'
+      rss.channel.link = "#{base_url}"
 
-  rss = RSS::Maker.make('2.0') do |rss|
-    rss.channel.title = "#{get_settings.filtering_regexp_str} - turifo"
-    rss.channel.description = errors.size > 0 ? errors.join("\n") : 'There is no error.'
-    rss.channel.link = "#{base_url}"
-
-    entries.each do |entry|
-      item = rss.items.new_item
-      item.title = entry.title
-      item.link = entry.url
-      item.guid.content = entry.id
-      # 元のフィードでisPermaLinkがどうだったかは保存されていない気がする(要出典)ので、一律でfalseにしておく
-      item.guid.isPermaLink = false
-      item.description = entry.content
-      item.date = entry.date_published
+      entries.each do |entry|
+        item = rss.items.new_item
+        item.title = entry.title
+        item.link = entry.url
+        item.guid.content = entry.id
+        # 元のフィードでisPermaLinkがどうだったかは保存されていない気がする(要出典)ので、一律でfalseにしておく
+        item.guid.isPermaLink = false
+        item.description = entry.content
+        item.date = entry.date_published
+      end
     end
-  end
 
-  content_type 'application/rss+xml', :charset => 'utf-8'
-  rss.to_s
+    content_type 'application/rss+xml', :charset => 'utf-8'
+    body rss.to_s
+  end
 end
 
 def get_settings
@@ -60,34 +63,48 @@ def get_settings
 end
 
 def get_entries_and_errors
-  errors = []
-  entries = get_settings.source_urls.map{|url|
-    feed = FeedNormalizer::FeedNormalizer.parse(open(url))
-    if feed
-      feed.entries.each{|entry|
-        # マージするので元のフィードが分かるようにentry.titleをいじっておく
-        entry.title = "#{entry.title} - #{feed.title}"
-        # 主に扱いたいカンパリのフィードのentry.date_publishedがぶっ壊れているので、代わりにfeed.last_updatedを突っ込んでおく
-        entry.date_published = feed.last_updated if (entry.date_published.year < 0)
-        # はてなアンテナのフィードなどはguidが存在しないため、適当なものを入れておく
-        entry.id = "#{entry.url}#{entry.date_published}" unless entry.id
-      }
-      feed.entries
-    else
-      errors << "Failed to fetch: #{url}"
-      []
-    end
-  }.flatten
-  entries = filter_entries(entries)
-  entries = entries.sort_by{|entry|
-    # entry.date_publishedが被っているケースがある。
-    # カンパリのフィードのentry.date_publishedを書き換えた場合などが該当する。
-    # そうしたときに少しでもそれらしい並びになるよう、entry.urlも考慮するようにしている。
-    # カンパリのフィードはentry.urlがオートインクリメントされているようなので、単一フィード内であればそれできれいに並ぶ。
-    "#{entry.date_published}#{entry.url}"
-  }.reverse
+  multi = EM::MultiRequest.new
 
-  return entries, errors
+  get_settings.source_urls.each do |url|
+    multi.add(url, EM::HttpRequest.new(url).get)
+  end
+
+  multi.callback do
+    entries = []
+    errors = []
+
+    multi.responses[:callback].each do |name, http|
+      feed = FeedNormalizer::FeedNormalizer.parse(http.response)
+      if feed
+        feed.entries.each{|entry|
+          # マージするので元のフィードが分かるようにentry.titleをいじっておく
+          entry.title = "#{entry.title} - #{feed.title}"
+          # 主に扱いたいカンパリのフィードのentry.date_publishedがぶっ壊れているので、代わりにfeed.last_updatedを突っ込んでおく
+          entry.date_published = feed.last_updated if (entry.date_published.year < 0)
+          # はてなアンテナのフィードなどはguidが存在しないため、適当なものを入れておく
+          entry.id = "#{entry.url}#{entry.date_published}" unless entry.id
+        }
+        entries.concat(feed.entries)
+      else
+        errors << "Failed to parse: #{http.req.uri}"
+      end
+    end
+
+    multi.responses[:errback].each do |name, http|
+      errors << "Failed to fetch: #{http.req.uri}(#{http.error})"
+    end
+
+    entries = filter_entries(entries)
+    entries = entries.sort_by{|entry|
+      # entry.date_publishedが被っているケースがある。
+      # カンパリのフィードのentry.date_publishedを書き換えた場合などが該当する。
+      # そうしたときに少しでもそれらしい並びになるよう、entry.urlも考慮するようにしている。
+      # カンパリのフィードはentry.urlがオートインクリメントされているようなので、単一フィード内であればそれできれいに並ぶ。
+      "#{entry.date_published}#{entry.url}"
+    }.reverse
+
+    yield entries, errors
+  end
 end
 
 def filter_entries(entries)
